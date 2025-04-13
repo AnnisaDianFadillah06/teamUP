93308daea5c967e5298942d751b92e90d66e9e98
Alya Nisrina <alyanisnst@gmail.com>
2025-04-13 15:16:46 +0700

    Memperbarui alur register, dan menambahkan halaman web verifikasi berhasil yang link email

--- app/src/main/java/com/example/teamup/route/NavGraph.kt
+++ app/src/main/java/com/example/teamup/route/NavGraph.kt
@@ -1,31 +1,42 @@
-package com.example.teamup.route
 
-import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.teamup.presentation.screen.ForgotPasswordScreen
-import com.example.teamup.presentation.screen.LoginScreenV5
-import com.example.teamup.presentation.screen.ProfileScreen
-import com.example.teamup.presentation.screen.RegisterScreen
-import com.example.teamup.presentation.screen.RegisterSuccessScreen
-import com.example.teamup.presentation.screen.ResetPasswordScreen
-import com.example.teamup.presentation.screen.VerificationScreen
-
-@Composable
-fun NavGraph(startDestination: String = Routes.Register.routes) {
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = startDestination) {
-        composable(Routes.Register.routes) { RegisterScreen(navController = navController) }
-        composable(Routes.LoginV5.routes) { LoginScreenV5(navController = navController) }
-        composable(Routes.ForgotPassword.routes) { ForgotPasswordScreen(navController = navController) }
-        composable(Routes.ResetPassword.routes) { ResetPasswordScreen(navController = navController) }
-
-        composable(Routes.Verification.routes) { VerificationScreen(navController = navController) }
-        composable(Routes.RegisterSuccess.routes) { RegisterSuccessScreen(navController = navController) }
-        composable(Routes.Profile.routes) { ProfileScreen(navController = navController) }
-
-        // Tambahkan route lain jika diperlukan
-    }
-}
-
+//package com.example.teamup.route
+//
+//import androidx.compose.runtime.Composable
+//import androidx.navigation.NavType
+//import androidx.navigation.compose.NavHost
+//import androidx.navigation.compose.composable
+//import androidx.navigation.compose.rememberNavController
+//import androidx.navigation.navArgument
+//import com.example.teamup.presentation.screen.*
+//import com.example.teamup.presentation.screen.register.CekEmailScreen
+//import com.example.teamup.presentation.screen.register.RegisterScreen
+//import com.example.teamup.presentation.screen.register.VerificationScreen
+//import com.example.teamup.presentation.screen.register.RegisterSuccessScreen
+//@Composable
+//fun NavGraph(startDestination: String = Routes.Register.routes) {
+//    val navController = rememberNavController()
+//    NavHost(navController = navController, startDestination = startDestination) {
+//        composable(Routes.Register.routes) { RegisterScreen(navController = navController) }
+//        composable(Routes.Login.routes) { LoginScreen(navController = navController) }
+//        composable(Routes.ForgotPassword.routes) { ForgotPasswordScreen(navController = navController) }
+//        composable(Routes.ResetPassword.routes) { ResetPasswordScreen(navController = navController) }
+//
+//        composable(Routes.Verification.routes) { VerificationScreen(navController = navController) }
+//        composable(Routes.RegisterSuccess.routes) { RegisterSuccessScreen(navController = navController) }
+//        composable(Routes.Profile.routes) { ProfileScreen(navController = navController) }
+//
+//        composable(
+//            route = Routes.CekEmail.routes,
+//            arguments = listOf(
+//                navArgument("email") {
+//                    type = NavType.StringType
+//                }
+//            )
+//        ) { backStackEntry ->
+//            val email = backStackEntry.arguments?.getString("email") ?: ""
+//            CekEmailScreen(navController, email = email)
+//        }
+//        // Tambahkan route lain jika diperlukan
+//    }
+//}
+//
