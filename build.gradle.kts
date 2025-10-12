0b566c6846557345fc823211d96573d04116dc3b
Alya Nisrina <alyanisnst@gmail.com>
2025-10-12 11:28:26 +0700

    gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,14 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    // Google Services Plugin (Latest Version)
     // Add the dependency for the Google services Gradle plugin
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+
 }
+
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
@@ -15,3 +20,8 @@ buildscript {
 }
 
 
+
+// Clean task
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
